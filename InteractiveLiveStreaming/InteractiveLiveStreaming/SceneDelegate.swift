e4702fb1282bfd43534d636f38f9eb2efc1824a9
bradley <kien.hoang@c0x12c.com>
2023-06-15 21:05:42 +0700

    Interactive Live Streaming

---
+++ InteractiveLiveStreaming/InteractiveLiveStreaming/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  InteractiveLiveStreaming
+//
+//  Created by Bradley Hoang on 14/06/2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(windowScene: windowScene)
+    window?.rootViewController = SDKQuickstartViewController()
+    window?.makeKeyAndVisible()
+  }
+}
+
--- Podfile
+++ Podfile
@@ -2,8 +2,6 @@ platform :ios, '14.0'
 
 workspace 'AgoraDemo.xcworkspace'
 
-project 'BroadcastStreaming/BroadcastStreaming.xcodeproj'
-
 def use_agora
   pod 'AgoraRtcEngine_iOS', '~> 4.2.0'
   pod 'AgoraUIKit_iOS', '~> 4.1.1'
@@ -11,6 +9,13 @@ end
 
 target 'BroadcastStreaming' do
   project 'BroadcastStreaming/BroadcastStreaming.xcodeproj'
+  use_frameworks!
+
+  use_agora
+end
+
+target 'InteractiveLiveStreaming' do
+  project 'InteractiveLiveStreaming/InteractiveLiveStreaming.xcodeproj'
   use_frameworks!
 
   use_agora
